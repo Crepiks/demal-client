09391867fd8f8e1a142164485a45c220d5c9ce3e
Azatyk <azatuk2005@mail.ru>
2021-04-24 19:30:47 +0600

    feat: add mock data

--- src/data/userEvents.js
+++ src/data/userEvents.js
@@ -2,51 +2,51 @@ export default {
   participatedEvents: [
     {
       id: 1,
-      title: "5-дневная сборка Frontend разработчиков",
+      title: "Экскурсия за Москвой",
       lat: 54.039777,
       lon: 43.935073,
       description:
-        "Двухэтажный дом с 7 комнатами, отличным ремонтом и интернетом недалеко от озера. Также в доме есть мангал, при желании можно приготовить барбекю.",
+        "Мы побудем во множестве интересных мест возле нашей столицы, посмотрим на великие строения, посетим не одну деревню и полюбуемся красотой России. 5-часовая экскурсия - это отличная возможность узнать чуть больше о месте, в котором вы живете и ментально отдохнуть.",
       images: [
         {
           imagePath:
-            "https://www.magazindomov.ru/wp-content/uploads/2015/05/Pool-House-3-678x381.jpg",
+            "https://avatars.mds.yandex.net/get-zen_doc/3311877/pub_5f2d3139e3927c2d2cfcbaee_5f2d357859dc5a64a781d36c/scale_1200",
         },
         {
           imagePath:
-            "https://tvil.ru/blog/sites/blog/files/image/articles/2019_10/2265885.jpeg",
+            "https://lh3.googleusercontent.com/proxy/aV-NdTteqhe8gIeVaiFcqPHXVcFSr-jqfL_YX4EHCJ_aS7aoNfB49ImlMpxAnw2CELKLbgCoqZeSv-wcA_2NHXpxrtYZDk2Rr4oRUQC4mjrPAmAFVd1cqIIo_iT0kcrXPU-nVHx6l2AlbpZh-snR7E1tvQ",
         },
         {
           imagePath:
-            "https://f9.mirkvartir.me/journal/custom/d00248da-7790-49ab-acc4-8e977ab50cda.jpg",
+            "https://avatars.mds.yandex.net/get-zen_doc/1537151/pub_5f2d3139e3927c2d2cfcbaee_5f2d353a6e602e1b309cae1b/scale_1200",
         },
         {
           imagePath:
-            "http://housefasad.ru/images/00000000000000000000000000000000000000000000000000000000000000000000000000000000000000001245845.jpg",
+            "https://tatmuseum.ru/wp-content/uploads/2014/07/DSC_0345.jpg",
         },
         {
           imagePath:
-            "https://www.magazindomov.ru/wp-content/uploads/2015/05/Pool-House-3-678x381.jpg",
+            "https://images.inlearno.ru/events_preview/ba9f7cb858f3d33c8c66067906e51c72_big.jpg",
         },
         {
           imagePath:
-            "https://tvil.ru/blog/sites/blog/files/image/articles/2019_10/2265885.jpeg",
+            "https://tatmuseum.ru/wp-content/uploads/2014/08/SDC12753-1024x768.jpg",
         },
         {
           imagePath:
-            "https://f9.mirkvartir.me/journal/custom/d00248da-7790-49ab-acc4-8e977ab50cda.jpg",
+            "https://i.pinimg.com/564x/ac/6d/72/ac6d72f5056e8a9d85a64811cdff5113.jpg",
         },
         {
           imagePath:
-            "http://housefasad.ru/images/00000000000000000000000000000000000000000000000000000000000000000000000000000000000000001245845.jpg",
+            "https://media.kupo.la/thumbor/unsafe/preset/orig/images/2021/3/18/1616059571-60fa15da-531a-4a25-aef2-2c2e57b6f445-Vitoslavlitsy6-1[1].jpg",
         },
       ],
       participants: [
         {
           id: 1,
-          firstName: "Саяжан",
-          lastName: "Онласын",
-          email: "sayazhan@mail.ru",
+          firstName: "Амир",
+          lastName: "Албеков",
+          email: "amir@mail.ru",
         },
         {
           id: 2,
@@ -56,59 +56,179 @@ export default {
         },
         {
           id: 3,
-          firstName: "Тимур",
-          lastName: "Сейткалиев",
-          email: "tima.director@mail.ru",
+          firstName: "Айлара",
+          lastName: "Светланова",
+          email: "ailara12345@mail.ru",
         },
       ],
       creator: {
-        firstName: "Арсен",
-        lastName: "Асанов",
-        email: "arsen@mail.ru",
+        firstName: "Оксана",
+        lastName: "Иванова",
+        email: "oksana1975@mail.ru",
       },
-      start: "2021-04-05T14:24:01.000Z",
-      end: "2021-04-23T14:24:01.000Z",
-      price: 10000,
+      start: "2021-05-05T14:24:01.000Z",
+      end: "2021-05-06T14:24:01.000Z",
+      price: 3000,
     },
     {
       id: 2,
-      title: "Дом масленных дизайнеров, рисуем пейзажи",
+      title: "Загородный корпоратив",
       lat: 54.022098,
       lon: 44.215033,
       description:
-        "Небольшой дом с дружелюбными соседями. Тут тихо и очень красиво, рядом лес с грибами и дом расположен всего лишь в 20 минутах от города, считайте в нем. Свежий воздух, хорошая погода.",
+        "Проведем отличный корпоратив в дали от города. Свежий воздух, вкусная еда и прекрасная природа России. Проведите корпоратив по-новому",
       images: [
         {
           imagePath:
-            "https://f9.mirkvartir.me/journal/custom/d00248da-7790-49ab-acc4-8e977ab50cda.jpg",
+            "https://piterevent.ru/ssl/u/15/862bd7142904ff97bc8a4690b5d590/+/cGl0ZXJldmVudC5ydQ==!%D0%97%D0%B0%D0%B3%D0%BE%D1%80%D0%BE%D0%B4%D0%BD%D1%8B%D0%B9%20%D0%BA%D0%BE%D1%80%D0%BF%D0%BE%D1%80%D0%B0%D1%82%D0%B8%D0%B2%20%D0%BB%D0%B5%D1%82%D0%BE%D0%BC.jpg",
         },
         {
           imagePath:
-            "https://www.magazindomov.ru/wp-content/uploads/2015/05/Pool-House-3-678x381.jpg",
+            "https://catering-muscat.ru/img/blog/blog_corporate_out-1.jpg",
         },
         {
           imagePath:
-            "https://tvil.ru/blog/sites/blog/files/image/articles/2019_10/2265885.jpeg",
+            "https://www.activetravel.com.ua/wp-content/uploads/2019/02/1.jpg",
         },
         {
           imagePath:
-            "https://tvil.ru/blog/sites/blog/files/image/articles/2019_10/2265885.jpeg",
+            "http://boligr.ru/wp-content/uploads/2017/03/5-2-768x514.jpg",
         },
         {
           imagePath:
-            "https://f9.mirkvartir.me/journal/custom/d00248da-7790-49ab-acc4-8e977ab50cda.jpg",
+            "https://ifvremya.ru/wp-content/uploads/2020/10/korporativ-na-prirode3.jpg",
         },
         {
           imagePath:
-            "http://housefasad.ru/images/00000000000000000000000000000000000000000000000000000000000000000000000000000000000000001245845.jpg",
+            "https://lh3.googleusercontent.com/proxy/CoB7Wdk78cZcx58GWwo323aIc-67rhHmVFms73l43Nr8SIV20sJtFhK5WCE0cklt63dfAZh34zgF13y9VIWahPZzTPZu-2s3y0_M1GohT2vFIF0VHP0tM5bmvNCXCic9",
+        },
+        {
+          imagePath: "https://www.peski13.ru/upload/iblock/f3e/priroda.jpg",
         },
         {
           imagePath:
-            "http://housefasad.ru/images/00000000000000000000000000000000000000000000000000000000000000000000000000000000000000001245845.jpg",
+            "https://opt-1131384.ssl.1c-bitrix-cdn.ru/upload/iblock/ad6/ad655d6f195ab9ae5d6089b0b9e291ee.jpg?1530773362162236",
+        },
+      ],
+      participants: [
+        {
+          id: 1,
+          firstName: "Карбек",
+          lastName: "Алиев",
+          email: "kara@mail.ru",
+        },
+        {
+          id: 2,
+          firstName: "Иван",
+          lastName: "Бессонов",
+          email: "vanya@mail.ru",
+        },
+        {
+          id: 3,
+          firstName: "Тимур",
+          lastName: "Сейткалиев",
+          email: "tima.director@mail.ru",
+        },
+      ],
+      creator: {
+        firstName: "Артем",
+        lastName: "Голубев",
+        email: "artem@mail.ru",
+      },
+      start: "2021-04-23T14:24:01.000Z",
+      end: "2021-04-26T14:24:01.000Z",
+      price: 4000,
+    },
+  ],
+  createdEvents: [
+    {
+      id: 3,
+      title: "Рыбалка - все включено",
+      lat: 54.442293,
+      lon: 36.443358,
+      description:
+        "2 дня рыбалки со всем, что нужно для хорошей рыбалки. Лодка, удочки, сетки, палатки и даже фотоаппарат. Не беспокойтесь о инструментах, просто насладитесь рыбалкой",
+      images: [
+        {
+          imagePath:
+            "https://www.svoiludi.ru/images/tb/14853/fishing-tours-15759686358801_w687h357.jpg",
         },
         {
           imagePath:
-            "https://www.magazindomov.ru/wp-content/uploads/2015/05/Pool-House-3-678x381.jpg",
+            "https://lh3.googleusercontent.com/proxy/OKkJir7rgBcRChaqkgObpKdth-FmK9gUuW8OojUckJF53rarfHvVmzRrQGP4zplo9HO-dZgmm7ioOuHwZZ90W7m1d23wJC8h_N5vMFtL7fAZfciiATwtgBzDqTrt2-ZT",
+        },
+        {
+          imagePath:
+            "https://ae01.alicdn.com/kf/Hc2a0f68d71b540e5abde58fac2b6bdf0Y/There-is-improved-version-4-side-60-100-cm-fish-shrimp-crab-nylon-bait-fishing-net.jpg",
+        },
+        {
+          imagePath:
+            "http://zavidovo.kz/wp-content/uploads/2016/09/zimnie-palatki-dlya-rybalki-v-almate.jpg",
+        },
+        {
+          imagePath:
+            "https://lh3.googleusercontent.com/proxy/rJWyDG90eS479PA_bLeeRdbM9lpBo3IAs8FSMI34MOUYMNVu81FFwJafSwkYf8bu5lPPfBBubiQX6YN_6AJkgyjLfO2cLybK0kEzW3k",
+        },
+        {
+          imagePath: "https://borovoe.kz/upload/borovoe_rybalka.jpg",
+        },
+        {
+          imagePath:
+            "https://artwalls.com.ua/image/catalog/fpz/5c1bb0770b78bVF-35_o.jpg",
+        },
+        {
+          imagePath:
+            "https://timetrial.ru/upload/iblock/d99/d9907e25ccd2c81bd50c37989e8c3682.jpg",
+        },
+      ],
+      participants: [],
+      creator: {
+        firstName: "Сергей",
+        lastName: "Рыбалкин",
+        email: "loverybalka@gmail.com",
+      },
+      start: "2021-04-10T14:24:01.000Z",
+      end: "2021-05-02T14:24:01.000Z",
+      price: 7000,
+    },
+    {
+      id: 4,
+      title: "Экскурсия в Кимжа",
+      lat: 54.506198,
+      lon: 35.942223,
+      description:
+        "Кимжа - одна из самых красивых деревень России. Двух дневная экскурсия с проживанием - это отличная возможность полюбоавиться природой и прочувствовать настоящую русскую деревню.",
+      images: [
+        {
+          imagePath:
+            "https://golubino.org/wp-content/uploads/2020/01/%D0%93%D0%B5%D1%80%D0%BD%D0%B5%D1%82-%D0%9D%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0%D0%B9-2.jpg",
+        },
+        {
+          imagePath:
+            "https://lh3.googleusercontent.com/proxy/okdaQCn6CTN_yZWIenwtmeY70LoXRyOiezMuN_1HZzISeB7zPe8bhqxIvf3xPqlWJwBBEw8nAISZKa0pqWh4rCTq9otSL6ZrDogj7PFejFOQ3YhQJgWc",
+        },
+        {
+          imagePath:
+            "https://lh3.googleusercontent.com/proxy/jmSv7LhafTZmLtHzKfsabP43TkG540kO27lD3YsoAXL0HZYXinWkn56LP0rYDnxPbvfXDrs5cz1jrbpNAuRMJkTzsq4Mc70gUnvOHNYoMMtE6taW_5FL",
+        },
+        {
+          imagePath:
+            "https://avatars.mds.yandex.net/get-zen_doc/3137181/pub_5f21ef2d091b0b16b84d13a7_5f21f1726b597f2c72d501c7/scale_1200",
+        },
+        {
+          imagePath:
+            "http://ic.pics.livejournal.com/love_smirnova/76342246/331715/331715_1000.jpg",
+        },
+        {
+          imagePath: "https://cdn.dvinanews.ru/sv1uw52u/bjm9.jpg",
+        },
+        {
+          imagePath:
+            "https://f.otzyv.ru/f/11/12/92763/p09111722305615133022.jpg",
+        },
+        {
+          imagePath:
+            "https://im.kommersant.ru/Issues.photo/CORP/2020/10/02/KMO_177956_00415_1_t222_204445.jpg",
         },
       ],
       participants: [
@@ -132,155 +252,13 @@ export default {
         },
       ],
       creator: {
-        firstName: "Арсен",
-        lastName: "Асанов",
-        email: "arsen@mail.ru",
+        firstName: "Арина",
+        lastName: "Асаковна",
+        email: "arina@mail.ru",
       },
-      start: "2021-03-07T14:24:01.000Z",
-      end: "2021-04-12T14:24:01.000Z",
-      price: 1500,
+      start: "2021-06-07T14:24:01.000Z",
+      end: "2021-06-09T14:24:01.000Z",
+      price: 5500,
     },
-  ],
-  createdEvents: [
-    // {
-    //   id: 3,
-    //   title: "Дом в Ахлебинино, рядом с мельницей",
-    //   lat: 54.442293,
-    //   lon: 36.443358,
-    //   description:
-    //     "Двухэтажный дом с 7 комнотами, олтичным ремонтом и интернетом недалеко от озера. Также в доме есть мангал, при желании можно приготовить барбекю.",
-    //   images: [
-    //     {
-    //       imagePath:
-    //         "https://tvil.ru/blog/sites/blog/files/image/articles/2019_10/2265885.jpeg",
-    //     },
-    //     {
-    //       imagePath:
-    //         "https://www.magazindomov.ru/wp-content/uploads/2015/05/Pool-House-3-678x381.jpg",
-    //     },
-    //     {
-    //       imagePath:
-    //         "https://f9.mirkvartir.me/journal/custom/d00248da-7790-49ab-acc4-8e977ab50cda.jpg",
-    //     },
-    //     {
-    //       imagePath:
-    //         "http://housefasad.ru/images/00000000000000000000000000000000000000000000000000000000000000000000000000000000000000001245845.jpg",
-    //     },
-    //     {
-    //       imagePath:
-    //         "https://www.magazindomov.ru/wp-content/uploads/2015/05/Pool-House-3-678x381.jpg",
-    //     },
-    //     {
-    //       imagePath:
-    //         "https://tvil.ru/blog/sites/blog/files/image/articles/2019_10/2265885.jpeg",
-    //     },
-    //     {
-    //       imagePath:
-    //         "https://f9.mirkvartir.me/journal/custom/d00248da-7790-49ab-acc4-8e977ab50cda.jpg",
-    //     },
-    //     {
-    //       imagePath:
-    //         "http://housefasad.ru/images/00000000000000000000000000000000000000000000000000000000000000000000000000000000000000001245845.jpg",
-    //     },
-    //   ],
-    //   participants: [
-    //     {
-    //       id: 1,
-    //       firstName: "Саяжан",
-    //       lastName: "Онласын",
-    //       email: "sayazhan@mail.ru",
-    //     },
-    //     {
-    //       id: 2,
-    //       firstName: "Аружан",
-    //       lastName: "Бесекова",
-    //       email: "aryka2003@mail.ru",
-    //     },
-    //     {
-    //       id: 3,
-    //       firstName: "Тимур",
-    //       lastName: "Сейткалиев",
-    //       email: "tima.director@mail.ru",
-    //     },
-    //   ],
-    //   creator: {
-    //     firstName: "Арсен",
-    //     lastName: "Асанов",
-    //     email: "arsen@mail.ru",
-    //   },
-    //   start: "2021-04-10T14:24:01.000Z",
-    //   end: "2021-05-02T14:24:01.000Z",
-    //   price: 5000,
-    // },
-    // {
-    //   id: 4,
-    //   title: "Загородный дом в Бобыниском районе, есть храм",
-    //   lat: 54.506198,
-    //   lon: 35.942223,
-    //   description:
-    //     "Двухэтажный дом с 7 комнотами, олтичным ремонтом и интернетом недалеко от озера. Также в доме есть мангал, при желании можно приготовить барбекю.",
-    //   images: [
-    //     {
-    //       imagePath:
-    //         "https://www.magazindomov.ru/wp-content/uploads/2015/05/Pool-House-3-678x381.jpg",
-    //     },
-    //     {
-    //       imagePath:
-    //         "https://tvil.ru/blog/sites/blog/files/image/articles/2019_10/2265885.jpeg",
-    //     },
-    //     {
-    //       imagePath:
-    //         "https://f9.mirkvartir.me/journal/custom/d00248da-7790-49ab-acc4-8e977ab50cda.jpg",
-    //     },
-    //     {
-    //       imagePath:
-    //         "http://housefasad.ru/images/00000000000000000000000000000000000000000000000000000000000000000000000000000000000000001245845.jpg",
-    //     },
-    //     {
-    //       imagePath:
-    //         "https://www.magazindomov.ru/wp-content/uploads/2015/05/Pool-House-3-678x381.jpg",
-    //     },
-    //     {
-    //       imagePath:
-    //         "https://tvil.ru/blog/sites/blog/files/image/articles/2019_10/2265885.jpeg",
-    //     },
-    //     {
-    //       imagePath:
-    //         "https://f9.mirkvartir.me/journal/custom/d00248da-7790-49ab-acc4-8e977ab50cda.jpg",
-    //     },
-    //     {
-    //       imagePath:
-    //         "http://housefasad.ru/images/00000000000000000000000000000000000000000000000000000000000000000000000000000000000000001245845.jpg",
-    //     },
-    //   ],
-    //   participants: [
-    //     {
-    //       id: 1,
-    //       firstName: "Саяжан",
-    //       lastName: "Онласын",
-    //       email: "sayazhan@mail.ru",
-    //     },
-    //     {
-    //       id: 2,
-    //       firstName: "Аружан",
-    //       lastName: "Бесекова",
-    //       email: "aryka2003@mail.ru",
-    //     },
-    //     {
-    //       id: 3,
-    //       firstName: "Тимур",
-    //       lastName: "Сейткалиев",
-    //       email: "tima.director@mail.ru",
-    //     },
-    //   ],
-    //   creator: {
-    //     firstName: "Арсен",
-    //     lastName: "Асанов",
-    //     email: "arsen@mail.ru",
-    //   },
-    //   start: "2021-05-07T14:24:01.000Z",
-    //   end: "2021-06-17T14:24:01.000Z",
-    //   price: 3000,
-    // },
   ],
 };
