a4c5215e79f25b15892fb0d2dd20f26afc8b8fc5
Crepiks <sayazhan.onlassyn@mail.ru>
2021-05-08 22:39:05 +0600

    feat: update mock tours list

--- src/data/events.js
+++ src/data/events.js
@@ -9,26 +9,23 @@ export default [
     images: [
       {
         id: 1,
-        path:
-          "https://thumb.tildacdn.com/tild3662-3831-4466-b237-396532336138/-/resize/800x1200/-/format/webp/img_2952-768x576.jpg",
+        path: "https://static.demal.space/1-1.jpeg",
       },
       {
         id: 2,
-        path:
-          "https://thumb.tildacdn.com/tild3365-6362-4238-b064-313465343165/-/format/webp/maxresdefault.jpg",
+        path: "https://static.demal.space/1-2.jpeg",
       },
       {
         id: 3,
-        path: "https://i.ytimg.com/vi/0bj4buu5iXs/maxresdefault.jpg",
+        path: "https://static.demal.space/1-3.jpeg",
       },
       {
         id: 4,
-        path: "https://www.free-writer.ru/files/2012/10/IMG_7300s.jpg",
+        path: "https://static.demal.space/1-4.jpeg",
       },
       {
         id: 5,
-        path:
-          "https://sletaem.kz/upload/iblock/a7a/a7a0a614b70d532b79daa0e4f43aa255.jpg",
+        path: "https://static.demal.space/1-5.jpeg",
       },
     ],
     participants: [
@@ -62,34 +59,31 @@ export default [
   },
   {
     id: 2,
-    title: "Однодневный тур на озёра Кольсай - Каинды и Чёрный каньон",
+    title: "Плато Ассы и Тургенский водопад",
     lat: 54.039777,
     lon: 43.935073,
     description:
-      "Тренд этого года: озёра Кольсай и Каинды. Эти места заставят Вас в них влюбиться и возвращаться к ним снова и снова.\n Приглашаем Вас увидеть воочию озёра Кольсай и Каинды, а также посетить Чёрный каньон — идеальный вариант для релакса и единения с природой, так как на локациях Вы будете свободны от всемирной паутины интернета. Бери друзей, семью и любимых, чтобы провести с ними выходной в природной сказке.",
+      "Плато Ассы и Тургеньский водопад . Сезон поездок на самый красивый жайлау открыт.\n Прекрасный уголок природы - Платто Ассы. Где открывается красивый до невозможности вид. Чистейший воздух, легкий ветерок и величественные горы. Душевная и атмосферная обстановка , интересные люди и душевные рассказы. Сделаем красивые фото и крутые видео . Это тот самый отдых, который нужен тебе. Долго думать не нужно, поехали с нами!",
     images: [
       {
         id: 1,
-        path:
-          "https://thumb.tildacdn.com/tild3662-3831-4466-b237-396532336138/-/resize/800x1200/-/format/webp/img_2952-768x576.jpg",
+        path: "https://static.demal.space/2-1.jpeg",
       },
       {
         id: 2,
-        path:
-          "https://thumb.tildacdn.com/tild3365-6362-4238-b064-313465343165/-/format/webp/maxresdefault.jpg",
+        path: "https://static.demal.space/2-2.jpeg",
       },
       {
         id: 3,
-        path: "https://i.ytimg.com/vi/0bj4buu5iXs/maxresdefault.jpg",
+        path: "https://static.demal.space/2-3.jpeg",
       },
       {
         id: 4,
-        path: "https://www.free-writer.ru/files/2012/10/IMG_7300s.jpg",
+        path: "https://static.demal.space/2-4.jpeg",
       },
       {
         id: 5,
-        path:
-          "https://sletaem.kz/upload/iblock/a7a/a7a0a614b70d532b79daa0e4f43aa255.jpg",
+        path: "https://static.demal.space/2-5.jpeg",
       },
     ],
     participants: [
@@ -119,38 +113,35 @@ export default [
     },
     start: "2021-05-05T14:24:01.000Z",
     end: "2021-05-06T14:24:01.000Z",
-    price: 3000,
+    price: 6000,
   },
   {
     id: 3,
-    title: "Однодневный тур на озёра Кольсай - Каинды и Чёрный каньон",
+    title: "Сплав по реке Или",
     lat: 54.039777,
     lon: 43.935073,
     description:
-      "Тренд этого года: озёра Кольсай и Каинды. Эти места заставят Вас в них влюбиться и возвращаться к ним снова и снова.\n Приглашаем Вас увидеть воочию озёра Кольсай и Каинды, а также посетить Чёрный каньон — идеальный вариант для релакса и единения с природой, так как на локациях Вы будете свободны от всемирной паутины интернета. Бери друзей, семью и любимых, чтобы провести с ними выходной в природной сказке.",
+      "ДЛИТЕЛЬНОСТЬ СПЛАВА 3,5 часа на воде. РАССТОЯНИЕ СПЛАВА 23км. Остановка на Чертовом пальце и на Крепости КОЧЕВНИКИ. Финиш ниже Тамгалы-Тас 1км.",
     images: [
       {
         id: 1,
-        path:
-          "https://thumb.tildacdn.com/tild3662-3831-4466-b237-396532336138/-/resize/800x1200/-/format/webp/img_2952-768x576.jpg",
+        path: "https://static.demal.space/3-1.jpeg",
       },
       {
         id: 2,
-        path:
-          "https://thumb.tildacdn.com/tild3365-6362-4238-b064-313465343165/-/format/webp/maxresdefault.jpg",
+        path: "https://static.demal.space/3-2.jpeg",
       },
       {
         id: 3,
-        path: "https://i.ytimg.com/vi/0bj4buu5iXs/maxresdefault.jpg",
+        path: "https://static.demal.space/3-3.jpeg",
       },
       {
         id: 4,
-        path: "https://www.free-writer.ru/files/2012/10/IMG_7300s.jpg",
+        path: "https://static.demal.space/3-4.jpeg",
       },
       {
         id: 5,
-        path:
-          "https://sletaem.kz/upload/iblock/a7a/a7a0a614b70d532b79daa0e4f43aa255.jpg",
+        path: "https://static.demal.space/3-5.jpeg",
       },
     ],
     participants: [
@@ -180,38 +171,39 @@ export default [
     },
     start: "2021-05-05T14:24:01.000Z",
     end: "2021-05-06T14:24:01.000Z",
-    price: 3000,
+    price: 2000,
   },
   {
     id: 4,
-    title: "Однодневный тур на озёра Кольсай - Каинды и Чёрный каньон",
+    title: "Чарынский каньон + Бартогайское водохранилище",
     lat: 54.039777,
     lon: 43.935073,
     description:
-      "Тренд этого года: озёра Кольсай и Каинды. Эти места заставят Вас в них влюбиться и возвращаться к ним снова и снова.\n Приглашаем Вас увидеть воочию озёра Кольсай и Каинды, а также посетить Чёрный каньон — идеальный вариант для релакса и единения с природой, так как на локациях Вы будете свободны от всемирной паутины интернета. Бери друзей, семью и любимых, чтобы провести с ними выходной в природной сказке.",
+      "Предлагаем Вам вместе с нами насладиться великолепным видами на каньоны и Бартогайское водохранилище. Мы успеем прогуляться по Долине Замков, сделать кучу крутых фото на обзорных площадках и понаблюдаем за бурной рекой Чарын, у которой остановимся на пикник. Это место заставит Вас в него влюбиться и возвращаться к нему снова и снова. Бери друзей, семью и любимых, чтобы провести с ними выходной в природной сказке. Чарынский каньон протянулся на 154 км вдоль реки Чарын. Удивительный памятник природы сложен из осадочных пород, возраст которых составляет порядка 12 миллионов лет. Высота достигает от 150 до 300 метров. Интересный факт: Чарынский каньон называют младшим братом Гранд-Каньона за счёт своей уникальной флоры и образования. Поэтому Вам обязательно стоит увидеть каньоны своими глазами",
     images: [
       {
         id: 1,
-        path:
-          "https://thumb.tildacdn.com/tild3662-3831-4466-b237-396532336138/-/resize/800x1200/-/format/webp/img_2952-768x576.jpg",
+        path: "https://static.demal.space/4-1.jpeg",
       },
       {
         id: 2,
-        path:
-          "https://thumb.tildacdn.com/tild3365-6362-4238-b064-313465343165/-/format/webp/maxresdefault.jpg",
+        path: "https://static.demal.space/4-2.jpeg",
       },
       {
         id: 3,
-        path: "https://i.ytimg.com/vi/0bj4buu5iXs/maxresdefault.jpg",
+        path: "https://static.demal.space/4-3.jpeg",
       },
       {
         id: 4,
-        path: "https://www.free-writer.ru/files/2012/10/IMG_7300s.jpg",
+        path: "https://static.demal.space/4-4.jpeg",
       },
       {
         id: 5,
-        path:
-          "https://sletaem.kz/upload/iblock/a7a/a7a0a614b70d532b79daa0e4f43aa255.jpg",
+        path: "https://static.demal.space/4-5.jpeg",
+      },
+      {
+        id: 5,
+        path: "https://static.demal.space/4-6.jpeg",
       },
     ],
     participants: [
@@ -241,7 +233,7 @@ export default [
     },
     start: "2021-05-05T14:24:01.000Z",
     end: "2021-05-06T14:24:01.000Z",
-    price: 3000,
+    price: 5000,
   },
   {
     id: 5,
@@ -253,26 +245,23 @@ export default [
     images: [
       {
         id: 1,
-        path:
-          "https://thumb.tildacdn.com/tild3662-3831-4466-b237-396532336138/-/resize/800x1200/-/format/webp/img_2952-768x576.jpg",
+        path: "https://static.demal.space/1-1.jpeg",
       },
       {
         id: 2,
-        path:
-          "https://thumb.tildacdn.com/tild3365-6362-4238-b064-313465343165/-/format/webp/maxresdefault.jpg",
+        path: "https://static.demal.space/1-2.jpeg",
       },
       {
         id: 3,
-        path: "https://i.ytimg.com/vi/0bj4buu5iXs/maxresdefault.jpg",
+        path: "https://static.demal.space/1-3.jpeg",
       },
       {
         id: 4,
-        path: "https://www.free-writer.ru/files/2012/10/IMG_7300s.jpg",
+        path: "https://static.demal.space/1-4.jpeg",
       },
       {
         id: 5,
-        path:
-          "https://sletaem.kz/upload/iblock/a7a/a7a0a614b70d532b79daa0e4f43aa255.jpg",
+        path: "https://static.demal.space/1-5.jpeg",
       },
     ],
     participants: [
@@ -306,34 +295,31 @@ export default [
   },
   {
     id: 6,
-    title: "Однодневный тур на озёра Кольсай - Каинды и Чёрный каньон",
+    title: "Плато Ассы и Тургенский водопад",
     lat: 54.039777,
     lon: 43.935073,
     description:
-      "Тренд этого года: озёра Кольсай и Каинды. Эти места заставят Вас в них влюбиться и возвращаться к ним снова и снова.\n Приглашаем Вас увидеть воочию озёра Кольсай и Каинды, а также посетить Чёрный каньон — идеальный вариант для релакса и единения с природой, так как на локациях Вы будете свободны от всемирной паутины интернета. Бери друзей, семью и любимых, чтобы провести с ними выходной в природной сказке.",
+      "Плато Ассы и Тургеньский водопад . Сезон поездок на самый красивый жайлау открыт.\n Прекрасный уголок природы - Платто Ассы. Где открывается красивый до невозможности вид. Чистейший воздух, легкий ветерок и величественные горы. Душевная и атмосферная обстановка , интересные люди и душевные рассказы. Сделаем красивые фото и крутые видео . Это тот самый отдых, который нужен тебе. Долго думать не нужно, поехали с нами!",
     images: [
       {
         id: 1,
-        path:
-          "https://thumb.tildacdn.com/tild3662-3831-4466-b237-396532336138/-/resize/800x1200/-/format/webp/img_2952-768x576.jpg",
+        path: "https://static.demal.space/2-1.jpeg",
       },
       {
         id: 2,
-        path:
-          "https://thumb.tildacdn.com/tild3365-6362-4238-b064-313465343165/-/format/webp/maxresdefault.jpg",
+        path: "https://static.demal.space/2-2.jpeg",
       },
       {
         id: 3,
-        path: "https://i.ytimg.com/vi/0bj4buu5iXs/maxresdefault.jpg",
+        path: "https://static.demal.space/2-3.jpeg",
       },
       {
         id: 4,
-        path: "https://www.free-writer.ru/files/2012/10/IMG_7300s.jpg",
+        path: "https://static.demal.space/2-4.jpeg",
       },
       {
         id: 5,
-        path:
-          "https://sletaem.kz/upload/iblock/a7a/a7a0a614b70d532b79daa0e4f43aa255.jpg",
+        path: "https://static.demal.space/2-5.jpeg",
       },
     ],
     participants: [
@@ -363,6 +349,126 @@ export default [
     },
     start: "2021-05-05T14:24:01.000Z",
     end: "2021-05-06T14:24:01.000Z",
-    price: 3000,
+    price: 6000,
+  },
+  {
+    id: 7,
+    title: "Сплав по реке Или",
+    lat: 54.039777,
+    lon: 43.935073,
+    description:
+      "ДЛИТЕЛЬНОСТЬ СПЛАВА 3,5 часа на воде. РАССТОЯНИЕ СПЛАВА 23км. Остановка на Чертовом пальце и на Крепости КОЧЕВНИКИ. Финиш ниже Тамгалы-Тас 1км.",
+    images: [
+      {
+        id: 1,
+        path: "https://static.demal.space/3-1.jpeg",
+      },
+      {
+        id: 2,
+        path: "https://static.demal.space/3-2.jpeg",
+      },
+      {
+        id: 3,
+        path: "https://static.demal.space/3-3.jpeg",
+      },
+      {
+        id: 4,
+        path: "https://static.demal.space/3-4.jpeg",
+      },
+      {
+        id: 5,
+        path: "https://static.demal.space/3-5.jpeg",
+      },
+    ],
+    participants: [
+      {
+        id: 1,
+        firstName: "Амир",
+        lastName: "Албеков",
+        email: "amir@mail.ru",
+      },
+      {
+        id: 2,
+        firstName: "Аружан",
+        lastName: "Бесекова",
+        email: "aryka2003@mail.ru",
+      },
+      {
+        id: 3,
+        firstName: "Айлара",
+        lastName: "Светланова",
+        email: "ailara12345@mail.ru",
+      },
+    ],
+    creator: {
+      firstName: "Оксана",
+      lastName: "Иванова",
+      email: "oksana1975@mail.ru",
+    },
+    start: "2021-05-05T14:24:01.000Z",
+    end: "2021-05-06T14:24:01.000Z",
+    price: 2000,
+  },
+  {
+    id: 8,
+    title: "Чарынский каньон + Бартогайское водохранилище",
+    lat: 54.039777,
+    lon: 43.935073,
+    description:
+      "Предлагаем Вам вместе с нами насладиться великолепным видами на каньоны и Бартогайское водохранилище. Мы успеем прогуляться по Долине Замков, сделать кучу крутых фото на обзорных площадках и понаблюдаем за бурной рекой Чарын, у которой остановимся на пикник. Это место заставит Вас в него влюбиться и возвращаться к нему снова и снова. Бери друзей, семью и любимых, чтобы провести с ними выходной в природной сказке. Чарынский каньон протянулся на 154 км вдоль реки Чарын. Удивительный памятник природы сложен из осадочных пород, возраст которых составляет порядка 12 миллионов лет. Высота достигает от 150 до 300 метров. Интересный факт: Чарынский каньон называют младшим братом Гранд-Каньона за счёт своей уникальной флоры и образования. Поэтому Вам обязательно стоит увидеть каньоны своими глазами",
+    images: [
+      {
+        id: 1,
+        path: "https://static.demal.space/4-1.jpeg",
+      },
+      {
+        id: 2,
+        path: "https://static.demal.space/4-2.jpeg",
+      },
+      {
+        id: 3,
+        path: "https://static.demal.space/4-3.jpeg",
+      },
+      {
+        id: 4,
+        path: "https://static.demal.space/4-4.jpeg",
+      },
+      {
+        id: 5,
+        path: "https://static.demal.space/4-5.jpeg",
+      },
+      {
+        id: 5,
+        path: "https://static.demal.space/4-6.jpeg",
+      },
+    ],
+    participants: [
+      {
+        id: 1,
+        firstName: "Амир",
+        lastName: "Албеков",
+        email: "amir@mail.ru",
+      },
+      {
+        id: 2,
+        firstName: "Аружан",
+        lastName: "Бесекова",
+        email: "aryka2003@mail.ru",
+      },
+      {
+        id: 3,
+        firstName: "Айлара",
+        lastName: "Светланова",
+        email: "ailara12345@mail.ru",
+      },
+    ],
+    creator: {
+      firstName: "Оксана",
+      lastName: "Иванова",
+      email: "oksana1975@mail.ru",
+    },
+    start: "2021-05-05T14:24:01.000Z",
+    end: "2021-05-06T14:24:01.000Z",
+    price: 5000,
   },
 ];
